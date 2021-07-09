c814bcee7756d1a5d4b16a886145f1e57a3422a5
Sarah Guidry <sarahbethguidry@gmail.com>
2021-07-09 02:56:35 -0500

    get posts are routed

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,25 @@
 // implement your posts router here
+
+const express = require('express')
+const Post = require('./posts-model')
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+	Post.find(req.query)
+		.then(posts => {
+			res.status(200).json(posts)
+		})
+		.catch(err => {
+			console.log(err)
+			res.status(500).json({
+				message: '######--Error retrieving the posts--######'
+			})
+		})
+})
+
+router.use(function (req, res, next) {
+	res.status(404).send("Sorry can't find that!")
+})
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,2 +1,19 @@
 // implement your server here
+const express = require('express')
 // require your posts router and connect it here
+const postsRouter = require('./posts/posts-router')
+
+const server = express()
+
+server.use(express.json())
+server.use('/api/posts', postsRouter)
+
+server.get('/', (req, res) => {
+	res.send(`
+	<h2>The Blogger API</h2>
+	<p>Welcome to The Blogger API</p>`)
+})
+
+module.exports = server
+
+
